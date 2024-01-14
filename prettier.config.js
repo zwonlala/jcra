feec7fe62901387e45de8aa9953e1e7b938a3b81
zwonlala <s26788761@naver.com>
2024-01-14 18:04:20 +0900

    #3 feat: prettier.config.js 추가 및 prettier 사용하는 명령어 package.json 추가

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: 'css',
+  printWidth: 150,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  tabWidth: 2,
+  quoteProps: 'as-needed',
+  trailingComma: 'es5',
+  useTabs: false,
+  endOfLine: 'auto',
+  importOrderSortSpecifiers: true,
+};
