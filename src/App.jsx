42746f71cf2946f165a404d110db682a450136fe
zwonlala <s26788761@naver.com>
2024-01-14 19:51:08 +0900

    test2

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import Timer from './Timer';
 
-;;;;;
-
-;;;;;
-
-
-
-
-
-
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
+        <Timer />
         <Timer />
         <Timer />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
+        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
           Learn React
         </a>
       </header>
