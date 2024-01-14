f047c8462cfa0bfb5d79d94c39953b13bd066745
zwonlala <s26788761@naver.com>
2024-01-14 19:51:41 +0900

    test3

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,7 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <Timer />
-        <Timer />
-        <Timer />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
