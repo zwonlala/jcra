14198cf14f23cbcc40ac830c2f57fa6c974ec089
zwonlala <s26788761@naver.com>
2024-01-15 15:36:42 +0900

    #3 feat: ES6+ 문법 정상적으로 transfile 되는지 체크

--- babel.config.js
+++ babel.config.js
@@ -18,14 +18,27 @@ module.exports = {
            */
         },
 
-        // 폴리필 속성 추가
+        /**
+         * preset-env 프리셋의 폴리필 삽입 방식을 설정하는 옵션
+         *
+         * - false 이외의 옵션: 최신 자바스크립트 폴리필이 포함된 standard javascript library인 core-js 모듈을 가져오는(import) 코드를 타깃 브라우저에 맞게 삽입/수정
+         *   - "entry" : core-js/stable과 regenerator-runtime/runtime 모듈을 전역 스코프에 직접 삽입한 경우에만 가능.
+         *                전체 core-js 삽입문(import)을 corejs 하위 특정 모듈들의 삽입문(import)으로 변경시켜, 타깃 환경에 필요한 폴리필만 전역 스코프에 추가되도록 함
+         *   - "usage" : 설정은 실제 사용한 폴리필만 삽입.
+         *                import 문 변경이 아닌 삽입이므로 core-js/stable과 regenerator-runtime/runtime 모듈을 전역 스코프에 삽입하지 않아도 됨
+         * - false
+         */
         useBuiltIns: 'usage', // 폴리필 사용 방식 지정
-        corejs: {
-          // 폴리필 버전 지정
-          version: 2,
-        },
+        /**
+         * 해당 옵션은 삽입되는 core-js 모듈의 버전을 설정
+         * 주의) useBuiltIns 옵션과 함께 사용해야 함.
+         */
+        corejs: '3',
       },
     ],
     ['@babel/preset-react', { runtime: 'automatic' }],
   ],
+  // plugins: [
+  //   ['@babel/plugin-transform-runtime', { corejs: 3 }]
+  // ],
 };
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 import Timer from './Timer';
 
+/** mock api url */
+const URL = 'https://yts.mx/api/v2/list_movies.json';
+
 function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await axios(URL);
+      console.log('SJW', data.body);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className='App'>
       <header className='App-header'>
