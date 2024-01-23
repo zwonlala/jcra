99394fed408c15080bc32fcad01b7770b60c0855
zwonlala <s26788761@naver.com>
2024-01-24 00:00:57 +0900

    #7 fix: vercel 배포시 시 빌드 실패하던 원인 제거...

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,4 @@
-const childProcess = require('child_process');
+// const childProcess = require('child_process');
 const path = require('path');
 
 const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // 주의! 구조분해 할당하여 CleanWebpackPlugin 사용하여야 함!
@@ -6,8 +6,8 @@ const webpack = require('webpack');
 
 const { version } = require('./package.json');
 
-const CMD_CLI_LAST_COMMIT = 'git rev-parse --short HEAD';
-const CMD_CLI_AUTHOR = 'git config user.name';
+// const CMD_CLI_LAST_COMMIT = 'git rev-parse --short HEAD';
+// const CMD_CLI_AUTHOR = 'git log -1 --pretty=format:"%an"';
 
 /**
  * "development" "production" "none" 중 하나
@@ -60,11 +60,9 @@ module.exports = {
   },
   plugins: [
     new webpack.BannerPlugin({
-      banner:
-        `Version: ${version}` +
-        `\nBuild Date: ${new Date().toLocaleString()}` +
-        `\nAuthor: ${childProcess.execSync(CMD_CLI_AUTHOR)}` +
-        `Last Commit: ${childProcess.execSync(CMD_CLI_LAST_COMMIT)}`,
+      banner: `Version: ${version}` + `\nBuild Date: ${new Date().toLocaleString()}`,
+      // `\nAuthor: ${childProcess.execSync(CMD_CLI_AUTHOR)}` +
+      // `Last Commit: ${childProcess.execSync(CMD_CLI_LAST_COMMIT)}`,
     }),
     new webpack.EnvironmentPlugin([]),
     /**
